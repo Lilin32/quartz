7aa0638827e02d3c12717d161fc1a6e130349076
yiscook <xfyan_3@foxmail.com>
2024-03-15 15:40:47 +0800

    修改 quartz 部分配置，添加 Linux扫盲 内容

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "yiscook's blog",
+    pageTitle: "🥘 yiscook's blog",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
